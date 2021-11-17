fc669fe6702b4c9983728153129813c6818eb768
vson <vson@itu.dk>
2021-11-17 10:08:05 +0100

    details

--- Node/Node.go
+++ Node/Node.go
@@ -4,135 +4,213 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	gh "github.com/SadlifeCaw/DisysyMandatory2/p2p"
-	"google.golang.org/grpc"
 	"log"
 	"net"
 	"os"
 	"strings"
 	"sync"
 	"time"
+
+	p2p "github.com/SadlifeCaw/DisysyMandatory2/p2p"
+	"google.golang.org/grpc"
 )
 
 type server struct {
-	gh.UnimplementedNodeServer
+	p2p.UnimplementedNodeServer
 }
 
+//different states of a Node
 const (
-	OUT = "OUT"
+	OUT        = "OUT"
 	REQUESTING = "REQUESTING"
-	CRITICAL = "CRITICAL"
+	CRITICAL   = "CRITICAL"
 )
 
-var (
-	status = OUT
-	port string
-	allowed = false
-	ports = make([]string, 0)
-	counter = 0
-	requestQueue = make ([]string, 0)
-	mu sync.Mutex
-)
+var TIME_TO_WAIT = 10 * time.Second
 
-func main(){
+var status = OUT
+var ownPort string
+var GrantedPermissionToEnter = false
+var counter = 0
+
+var knownPortsSlice = make([]string, 0)
+var requestQueue = make([]string, 0)
+
+var mu sync.Mutex
+
+func main() {
 	var file, _ = os.Open(os.Args[1])
+
 	var reader = bufio.NewReader(file)
 	var line, _ = reader.ReadString('\n')
+
+	//read given ports
 	line = strings.TrimSuffix(line, "\r\n")
-	ports = strings.Split(line, ";")
-	log.Printf("Ports: %s, %s", ports[0], ports[1])
+	knownPortsSlice = strings.Split(line, ";")
+	log.Printf("Other nodes exist at Ports: %s, %s", knownPortsSlice[0], knownPortsSlice[1])
+
+	//trim new line on last port
+	knownPortsSlice[1] = strings.TrimSuffix(knownPortsSlice[1], "\n")
+
+	//read own port
 	line, _ = reader.ReadString('\n')
 	line = strings.TrimSuffix(line, "\n")
-	port = line
+	ownPort = line
 
+	//set up logging to appropiate file
+	logPath := GetLoggerFileName(ownPort)
+
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	log.Println("-----------------------------------")
+	log.Println("Logging to custom file on port", ownPort)
+
+	//begin actual implementation
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go listen()
-	go queueHandler(ctx)
+	go ListenToPort()
+	go CheckQueue(ctx)
 
-	var input string
+	//give other Nodes enough time to start up
+	log.Printf("Waiting 10 seconds before beginning")
+	time.Sleep(TIME_TO_WAIT)
 
-	fmt.Scan(&input)
-	time.Sleep(time.Second * 10)
-
+	//keep trying to enter the critical section
+	//should maybe be tied to some listener for change in acceptance counter
 	for {
-		time.Sleep(time.Millisecond * 1) //Try with 10
+		time.Sleep(time.Second * 3)
 		attemptEnter(ctx)
-		time.Sleep(time.Second) //Try with 10
+		time.Sleep(time.Millisecond * 100)
 		leave(ctx)
 	}
 }
 
-func listen(){
-	lis, err := net.Listen("tcp", port)
+func ListenToPort() {
+	lis, err := net.Listen("tcp", ownPort)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
-	gh.RegisterNodeServer(s, &server{})
+	p2p.RegisterNodeServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
-func enqueue(queue []string, str string) []string {
-	return append(queue, str)
+func GetLoggerFileName(port string) (filename string) {
+	switch port {
+
+	case ":2000":
+		return "log_node1.txt"
+	case ":2001":
+		return "log_node2.txt"
+	case ":2002":
+		return "log_node3.txt"
+
+	default:
+		log.Printf("Error, illegal port while settign up Logger")
+		return "" //why? because go :(
+	}
 }
 
-func dequeue(queue []string) (string, []string){
+//queue implementation, could be abstracted to a struct/it's own file in the p2p folder
+func Enqueue(queue []string, port string) []string {
+	return append(queue, port)
+}
+
+func Dequeue(queue []string) (string, []string) {
 	if len(queue) != 0 {
-		dequeued := queue[0]
+		poppedElement := queue[0]
 		queue = queue[1:]
-		return dequeued, queue
+		return poppedElement, queue
 	} else {
 		return "", queue
 	}
 }
 
-func queueHandler(ctx context.Context) {
+//contionusly see if other Nodes have given acceptance for another Node to enter the critical section
+func CheckQueue(ctx context.Context) {
 	for {
-		if !allowed {
-			var allowPort string
+		//only loop if this Node has NOT been granted permission from the other Nodes
+		if !GrantedPermissionToEnter {
+			var NextPortToEnter string
+
 			mu.Lock()
-			allowPort, requestQueue = dequeue(requestQueue)
+			NextPortToEnter, requestQueue = Dequeue(requestQueue)
 			mu.Unlock()
-			if allowPort == "" {
+
+			//if no Node in queue, skip iteration
+			if NextPortToEnter == "" {
 				continue
 			}
-			allowPort = fmt.Sprintf("localhost%s", allowPort)
-			conn, _ := grpc.Dial(allowPort, grpc.WithInsecure(), grpc.WithBlock())
-			c := gh.NewNodeClient(conn)
+
+			NextPortToEnter = fmt.Sprintf("localhost%s", NextPortToEnter)
+
+			conn, _ := grpc.Dial(NextPortToEnter, grpc.WithInsecure(), grpc.WithBlock())
+			NextPortNode := p2p.NewNodeClient(conn)
+
 			mu.Lock()
-			allowed = true
+			GrantedPermissionToEnter = true
 			mu.Unlock()
-			c.Accept(ctx, &gh.AcceptMessage{})
+
+			//Increase this Node acceptance counter
+			//needs to happen once for every known Node before being GrantedPermissionToEnter in the critical section
+			NextPortNode.Accept(ctx, &p2p.AcceptMessage{})
 		}
 	}
 }
 
-func attemptEnter(ctx context.Context){
+//tell all known Nodes that this Node wants to enter
+func attemptEnter(ctx context.Context) {
 	status = REQUESTING
-	for i := 0; i < len(ports); i++ {
-		conn, _ := grpc.Dial(ports[i], grpc.WithInsecure())
-		c := gh.NewNodeClient(conn)
-		c.Request(ctx, &gh.RequestMessage{Node: port})
+
+	for i := 0; i < len(knownPortsSlice); i++ {
+		conn, err := grpc.Dial(knownPortsSlice[i], grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Could not connect: %s", err)
+		}
+
+		Node := p2p.NewNodeClient(conn)
+
+		//enqueue this Node in other Node's queue
+		Node.Request(ctx, &p2p.RequestMessage{Message: ownPort, Node: ownPort})
+
+		//close connection to other Node
+		conn.Close()
 	}
 
+	//check if this Node has been accepted by every other Node
 	for {
-		if counter == len(ports){
+		if counter == len(knownPortsSlice) {
 			mu.Lock()
-			counter = 0
+			counter = 0 //reset acceptance counter
 			mu.Unlock()
+
 			status = CRITICAL
-			log.Printf("Node with port %s has entered the critical state", port)
-			for  i := 0; i < len(ports); i++{
-				conn, _ := grpc.Dial(ports[i], grpc.WithInsecure(), grpc.WithBlock())
-				c := gh.NewNodeClient(conn)
-				c.EnterCritical(ctx, &gh.EnterMessage{Message: port})
+			log.Printf("Node with port %s : ENTER", ownPort)
+
+			for i := 0; i < len(knownPortsSlice); i++ {
+				//create connection to other Node
+				conn, _ := grpc.Dial(knownPortsSlice[i], grpc.WithInsecure(), grpc.WithBlock())
+				Node := p2p.NewNodeClient(conn)
+
+				//tell other Nodes this Node has entered the critical section
+				Node.EnterCritical(ctx, &p2p.EnterMessage{Message: ownPort})
+
+				//close connection to other Node
 				conn.Close()
 			}
+			//break loop
 			break
 		}
 	}
@@ -140,37 +218,48 @@ func attemptEnter(ctx context.Context){
 
 func leave(ctx context.Context) {
 	status = OUT
-	log.Printf("Node with port %s has left the critical state", port)
-	for i := 0; i < len(ports); i++ {
-		conn, _ := grpc.Dial(ports[i], grpc.WithInsecure(), grpc.WithBlock())
-		c := gh.NewNodeClient(conn)
-		c.LeaveCritical(ctx, &gh.LeaveMessage{Message: port})
+	log.Printf("Node with port %s : LEAVE", ownPort)
+
+	for i := 0; i < len(knownPortsSlice); i++ {
+
+		//create connection to other Node
+		conn, _ := grpc.Dial(knownPortsSlice[i], grpc.WithInsecure(), grpc.WithBlock())
+		Node := p2p.NewNodeClient(conn)
+
+		//inform other Node that this Node is leaving the critical section
+		Node.LeaveCritical(ctx, &p2p.LeaveMessage{Message: ownPort})
+
+		//close connection to other Node
+		conn.Close()
 	}
 }
 
-func (s *server) Request(ctx context.Context, in *gh.RequestMessage) (*gh.RequestReply, error) {
+//when requesting to enter the critical section, enqueue this Node
+func (s *server) Request(ctx context.Context, in *p2p.RequestMessage) (*p2p.RequestReply, error) {
 	mu.Lock()
-	requestQueue = enqueue(requestQueue, in.Node)
+	requestQueue = Enqueue(requestQueue, in.Node)
 	mu.Unlock()
-	return &gh.RequestReply{}, nil
+
+	return &p2p.RequestReply{}, nil
 }
 
-func (s *server) Accept(ctx context.Context, in *gh.AcceptMessage) (*gh.AcceptReply, error){
+func (s *server) Accept(ctx context.Context, in *p2p.AcceptMessage) (*p2p.AcceptReply, error) {
 	mu.Lock()
 	counter++
 	mu.Unlock()
-	return &gh.AcceptReply{}, nil
+
+	return &p2p.AcceptReply{}, nil
 }
 
-func (s *server) LeaveCritical(ctx context.Context, in *gh.LeaveMessage) (*gh.LeaveReply, error) {
-	log.Printf("Node with port %s has left the critical state", in.Message)
+func (s *server) LeaveCritical(ctx context.Context, in *p2p.LeaveMessage) (*p2p.LeaveReply, error) {
+	log.Printf("Node with port %s : LEAVE", in.Message)
 	mu.Lock()
-	allowed = false
+	GrantedPermissionToEnter = false
 	mu.Unlock()
-	return &gh.LeaveReply{}, nil
+	return &p2p.LeaveReply{}, nil
 }
 
-func (s *server) EnterCritical(ctx context.Context, in *gh.EnterMessage) (*gh.EmptyReply, error) {
-	log.Printf("Node with port %s, has entered", in.Message)
-	return &gh.EmptyReply{}, nil
+func (s *server) EnterCritical(ctx context.Context, in *p2p.EnterMessage) (*p2p.EmptyReply, error) {
+	log.Printf("Node with port %s : ENTER", in.Message)
+	return &p2p.EmptyReply{}, nil
 }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # DisysyMandatory2
+
+## Running the program
+
+The program is designed to be used with 3 nodes exactly.
+
+To run it, open 3 terminals and in each of them run the go module with a different input file.
+Below is an example with using node1.txt as input (use a different .txt file for each node, 1 to 3)
+
+```
+go run . ../node1.txt
+```
+
+**Important**: After a Node has been created by the command above, you have 10 seconds to activate every other node. If all Nodes are not initialized when the program properly starts, it will not function correctly.
+If this is too little time, you can change the variable "TIME_TO_WAIT" in Node/Node.go
+## Logging
+
+Each Node logs its operations to its relevant log file, which can be found in /Node
+A quick overview:
+
+- log_node1.txt logs from Port 2000
+
+- log_node2.txt logs from Port 2001
+
+- log_node3.txt logs from Port 2003
